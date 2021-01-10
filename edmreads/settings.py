d07cbd4b35c51766d739a6530f06dcc07200acc5
Rupin Bapuji <rupin142002@gmail.com>
2021-01-09 23:41:02 -0700

    redirect

--- edmreads/settings.py
+++ edmreads/settings.py
@@ -60,6 +60,9 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+
 ROOT_URLCONF = 'edmreads.urls'
 
 TEMPLATES = [
