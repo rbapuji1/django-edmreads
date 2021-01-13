3efed252822fa99dcd243f501127eb6cfecbbdb1
Rupin Bapuji <rupin142002@gmail.com>
2021-01-12 20:45:00 -0700

    cumulative holds feature

--- books/info.py
+++ books/info.py
@@ -6,7 +6,7 @@ def retrieve_epl():
     response = requests.get("https://data.edmonton.ca/resource/qdgm-hex6.json?$order=date%20DESC,%20holds%20DESC")
 
     if response.status_code == 200:
-        data = response.json()[0:10]
+        data = response.json()
         
         #if it is up to date dont pull new data
         if save.check_db(data[0]):
@@ -17,6 +17,29 @@ def retrieve_epl():
     else:
         return None
 
+def get_top_ten(data):
+    first_date = data[0]["date"]
+    
+    index = 0
+    holds_data = {}
+    while data[index]["date"] == first_date:
+        
+        if data[index]["title"] in holds_data:
+            holds_data["title"][1] += data[index]["holds"]
+        else:
+            holds_data[data[index]["title"]] = [data[index], data[index]["holds"]]
+
+        index += 1
+    
+    sorted_data = sorted(holds_data, key=holds_data.get()[1], reverse = True)
+  
+    data = [obj[0] for obj in sorted_data.values()][0:10]
+
+    return data
+    
+    
+
+    
 
 def change_title_author(data):
     for obj in data:
@@ -49,6 +72,7 @@ def retrieve_google(title, authour):
         else:
             dsc = "No description available"
 
+    """
     link = f"https://customsearch.googleapis.com/customsearch/v1?key=AIzaSyBMFZ8q7dLwkWbmlC94QRBdCr9ZzbyAgqA&cx=46c33dd388e2032df&imgSize=large&searchType=image&q={title} cover"
 
     img_rsp = requests.get(link)
@@ -57,8 +81,10 @@ def retrieve_google(title, authour):
         img_data = img_rsp.json()["items"][0]
 
         img = img_data["link"]
+    
     else:
-        img = "https://unmpress.com/sites/default/files/default_images/no_image_book.jpg"
+        """
+    img = "https://unmpress.com/sites/default/files/default_images/no_image_book.jpg"
 
     return dsc, img
     
@@ -77,6 +103,7 @@ def finalcheck():
     data = retrieve_epl()
 
     if data:
+        data = get_top_ten(data)
         change_title_author(data)
         new_data = combine(data)
         for obj in new_data:
